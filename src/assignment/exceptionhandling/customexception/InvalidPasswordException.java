478e57448c5e97c70eef3327e2aa0ee4aa924469
Aditya Kalambe <adityakalambe20@gmail.com>
2024-06-09 02:24:31 +0530

    Assignment of Exception

--- src/assignment/exceptionhandling/customexception/InvalidPasswordException.java
+++ src/assignment/exceptionhandling/customexception/InvalidPasswordException.java
@@ -1,7 +1,7 @@
 package assignment.exceptionhandling.customexception;
 
 public class InvalidPasswordException extends RuntimeException{
-    private String message = "password should contain 1 uppercase, 1 lowercase, 1 numeric, 1 special character";
+    private String message = "password should contain 1 uppercase, 1 lowercase, 1 numeric, 1 special character.";
 
     public InvalidPasswordException() {}
     public InvalidPasswordException(String message) {
